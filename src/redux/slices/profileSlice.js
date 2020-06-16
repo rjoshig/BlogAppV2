73841a7447caf067cfe5251756fe347837340cd2
Ramesh Joshi <30200211+rjoshig@users.noreply.github.com>
2020-06-16 05:51:21 -0500

    cleanup profile code

--- src/redux/slices/profileSlice.js
+++ src/redux/slices/profileSlice.js
@@ -16,30 +16,6 @@ import {
 export const readUserTable = createAsyncThunk('users/readUserTableStatus', async (uid) => {
   try {
     const userData = await ReadUserTableService(uid)
-    console.log('DEBUG:: \x1b[36m   readUserTable THUNK -> userData --> ', userData)
-    console.log('DEBUG:: \x1b[36m   readUserTable THUNK -> userData --> ', userData.val())
-    //  const profileData = { userData }
-    // const profileData1 = {
-    //   aboutme: userData.aboutme,
-    //   displayName: userData.displayName,
-    //   email: userData.email,
-    //   uid: userData.uid,
-    // }
-    // // console.log('DEBUG:: readUserTable -> profileData', profileData)
-    // console.log('DEBUG:: readUserTable -> profileData1', profileData1)
-
-    //   if (userData) {
-    //     const user = {
-    //       fullName: userData.user.displayName,
-    //       emailId: userData.user.email,
-    //       photoUrl: userData.user.photoUrl,
-    //       emailVerified: userData.user.emailVerified,
-    //       uid: userData.user.uid,
-    //     }
-    //     console.log('THUNK USER', user)
-    //     return user
-    //   }
-
     return userData.val() // .val is needed because Thunk will throw error serialiazable error
   } catch (err) {
     const resMessage =
@@ -54,7 +30,7 @@ export const readUserTable = createAsyncThunk('users/readUserTableStatus', async
 // INITIAL AUTH STATE
 const initialState = {
   userTableData: {},
-  isReAuthenticateFormVisible: false, //! start from here
+  isReAuthenticateFormVisible: false,
   isNewPasswordFormVisible: false,
   // aboutMe
 }
@@ -70,13 +46,11 @@ const profileSlice = createSlice({
       state.isNewPasswordFormVisible = action.payload // true/false
     },
   },
-  // thunk can be called here
+
   extraReducers: {
     [readUserTable.pending]: (state, action) => {},
     [readUserTable.rejected]: (state, action) => {},
     [readUserTable.fulfilled]: (state, action) => {
-      console.log('DEBUG:: action', action)
-
       state.userTableData = action.payload
     },
   },
@@ -84,32 +58,8 @@ const profileSlice = createSlice({
 
 // actions generated from the slice
 export const { setIsReAuthenticateFormVisible, setIsNewPasswordFormVisible } = profileSlice.actions
-// const authReducer = authSlice.reducer
+
 // A selector
 export const profileSelector = (state) => state.profile // This exports the state so that useSelector Hook can use it in components
 // The reducer
 export default profileSlice.reducer
-
-// createSlice returns an object that looks like this:
-// {
-//   name: "todos",
-//   reducer: (state, action) => newState,
-//   actions: {
-//     addTodo: (payload) => ({type: "todos/addTodo", payload}),
-//     toggleTodo: (payload) => ({type: "todos/toggleTodo", payload})
-//   },
-//   caseReducers: {
-//     addTodo: (state, action) => newState,
-//     toggleTodo: (state, action) => newState,
-//   }
-// }
-
-// import auth from '@react-native-firebase/auth'
-// thunk
-
-// export const onAuthStateChanged = createAsyncThunk('users/onAuthStateChangedStatus', async () => {
-
-//   const response =  await auth().onAuthStateChanged((user) => {return user})
-
-//   return response
-// })
